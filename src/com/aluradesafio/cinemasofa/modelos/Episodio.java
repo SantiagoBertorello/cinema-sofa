a97a1043536e631fed32d1d76c241bb3f50b6c26
Santiago Bertorello <santinbertorello@gmail.com>
2025-05-03 17:21:11 -0300

    add: clase episodio

---
+++ src/com/aluradesafio/cinemasofa/modelos/Episodio.java
@@ -0,0 +1,35 @@
+package com.aluradesafio.cinemasofa.modelos;
+
+public class Episodio {
+
+    private int numero;
+
+    private String nombre;
+
+    private String serie;
+
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSerie() {
+        return serie;
+    }
+
+    public void setSerie(String serie) {
+        this.serie = serie;
+    }
+}
