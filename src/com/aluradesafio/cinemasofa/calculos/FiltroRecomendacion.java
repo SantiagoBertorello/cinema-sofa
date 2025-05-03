22fffc444821b934f3783b0051fc724166c484be
Santiago Bertorello <santinbertorello@gmail.com>
2025-05-03 17:52:42 -0300

    add: creacion de clase FiltroRecomendacion

---
+++ src/com/aluradesafio/cinemasofa/calculos/FiltroRecomendacion.java
@@ -0,0 +1,15 @@
+package com.aluradesafio.cinemasofa.calculos;
+
+public class FiltroRecomendacion {
+
+    public void filtra(Clasificacion clasificacion){
+        if(clasificacion.getClasificacion() >= 4){
+            System.out.println("Pelicula con buena evaluación");
+        }else if(clasificacion.getClasificacion() >= 2){
+            System.out.println("popular en el momento");
+        }else {
+            System.out.println("Guardala en tu lista para ver más tarde.");
+        }
+
+    }
+}
